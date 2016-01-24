4af246d5b5900dcf217c5b3552fa2fc1e058ae40
swapnilbhatt <swapnil.bhatt15@gmail.com>
2016-01-24 14:07:45 -0500

    elaborated on commit

--- count.sh
+++ count.sh
@@ -1,5 +1,6 @@
 #display the name of each data file and count
-#the number pof distrinct occurences of each species
+#the number pof distrinct occurences of each species. Results are displayed in alphabatical order
+#by spicies name.
 # n that file
 for data in *.txt
 	do
